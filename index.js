8159b9f482a10f536b477bdcebcbbf4b52c14bb0
Dylan Townsend <dylantowna@gmail.com>
2021-03-29 18:59:32 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 var rpc = require("discord-rpc")
-const client = new rpc.Client({ transport: 'ipc' })
+const client = new rpc.Client({
+    transport: 'ipc'
+})
 client.on('ready', () => {
-client.request('SET_ACTIVITY', {
-pid: process.pid,
-activity : {
-details : "🧞View My Website",
-assets : {
-large_image : "large2",
-large_text : "💻Coding Discord Bots" 
-},
-buttons : [{label : "Discord" , url : "https://discord.gg/wetxR93SFZ"},{label : "Website",url : "https://www.gwopdevelopment.com/"}]
-}
+    client.request('SET_ACTIVITY', {
+        pid: process.pid,
+        activity: {
+            details: "🧞View My Website",
+            assets: {
+                large_image: "large2",
+                large_text: "💻Coding Discord Bots"
+            },
+            buttons: [{
+                label: "Discord",
+                url: "https://discord.gg/wetxR93SFZ"
+            }, {
+                label: "Website",
+                url: "https://www.gwopdevelopment.com/"
+            }]
+        }
+    })
 })
-})
-client.login({ clientId : "817003264524288010" }).catch(console.error);
+client.login({
+    clientId: "817003264524288010"
+}).catch(console.error);
